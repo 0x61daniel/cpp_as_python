13348c5803cc6442f4aba51d06a95cc9ad274662
lzj <623968447@qq.com>
2019-09-08 20:01:26 +0800

    20190908

--- PyString.h
+++ PyString.h
@@ -1,5 +1,5 @@
-#ifndef __ENHANCESTRING__
-#define __ENHANCESTRING__
+#ifndef __PYSTRING__
+#define __PYSTRING__
 
 #include <climits>
 
@@ -30,37 +30,44 @@ class PyString : public std::string
 public:
     PyString(const char *s) : std::string(s) { };   //委派构造函数
     PyString(const std::string s) : std::string(s) { };
+    
     bool isdigit();
     bool isalpha();
     bool isalnum();
     bool isspace();
-    bool startswith(std::string substr, int beg=0,int end=INT_MAX);
-    int find(const std::string& subStr, int beg=0,int end=INT_MAX);
-    int count(const std::string& subStr, int beg=0,int end=INT_MAX);
-    std::vector<std::string>& split(const char delim, int maxsplit=-1, const int rep=false);
-    std::vector<std::string>& rsplit(const char delim, int maxsplit=-1, const int rep=false);
+    bool startswith(std::string substr, int start=0, int end=INT_MAX);
+    bool endswith(std::string substr, int start=0, int end=INT_MAX);
 
+    int find(const std::string& subStr, int start=0, int end=INT_MAX);
+    int rfind(const std::string& subStr, int start=0, int end=INT_MAX);
+    int count(const std::string& subStr, int start=0, int end=INT_MAX);
+
+    template <typename... Types>
+    PyString format(const Types&... args);
     PyString strip(const std::string str);
     PyString lstrip(const std::string str);
     PyString rstrip(const std::string str);
     PyString replace_py(const std::string sourceStr, const std::string targetStr, int iMaxReplace=-1);
 
-    template <typename... Types>
-    PyString format(const Types&... args);
+    std::vector<std::string>& split(const char delim, int maxsplit=-1, const int rep=false);
+    std::vector<std::string>& rsplit(const char delim, int maxsplit=-1, const int rep=false);
 private:
     static std::vector<std::string> flds;
-    PyString __strip(const std::string ch, const bool lstrip=false, const bool rstrip=false);
-    void BuildFormatString(std::ostringstream& builder, const std::string& fmt_spec, std::string::size_type idx);
-    template <typename T, typename... Types>
-    void BuildFormatString(std::ostringstream& builder, const std::string& fmt_spec,
-                           std::string::size_type idx, const T& first,
-                           const Types&... args);
+
     int ConvertInterval(const int& strSize, const int& subStrSize, int& start, int& end);
+    int __find(const std::string& subStr, int start, int end, bool rfind);
+
     template<typename T>
     bool __split(T& pIndex,  T& pEnd, const char& delim, int maxsplit, const bool rsplit, const bool rep);
     bool __isdigit(const char ch);
     bool __isalpha(const char ch);
+
+    template <typename T, typename... Types>
+    void BuildFormatString(std::ostringstream& builder, const std::string& fmt_spec,
+                           std::string::size_type idx, const T& first, const Types&... args);
+    void BuildFormatString(std::ostringstream& builder, const std::string& fmt_spec, std::string::size_type idx);
     
+    PyString __strip(const std::string ch, const bool lstrip=false, const bool rstrip=false);
 };
 
 
