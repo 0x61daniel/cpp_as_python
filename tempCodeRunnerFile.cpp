876f9733f88b8bb7ffe50f9ae975bca0e7f0791e
lzj <623968447@qq.com>
2019-10-23 23:50:36 +0800

    update

--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-py_find("ll") << endl;
-    // cout << str.py_rfind("ll") << endl;
-    // cout << str.py_rfind("ll", 0, 2) << endl;
-    // cout << str.py_find("ll", 0, 4) << endl;
-
-    // cout << str.py_count("l") << endl;
